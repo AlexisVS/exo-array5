eb81b21f27ed78b7eb9f9e2c10dfe1c8abde05ec
AlexisVS <63750707+AlexisVS@users.noreply.github.com>
2021-04-21 01:22:53 +0200

    Add files via upload

---
+++ exo-array5/public/js/main.js
@@ -0,0 +1,40 @@
+//exo 1
+let ok = "ok"
+let tabExo = ["0", ok, 2, 3, true];
+console.log(tabExo);
+//exo 2
+console.log(tabExo.length);
+//exo 3
+tabExo.pop();
+console.log(tabExo);
+//exo 4
+tabExo.shift();
+console.log(tabExo);
+//exo 5
+tabExo.push(4, 5);
+console.log(tabExo);
+//exo 6.
+tabExo.unshift(1, 2, 3);
+console.log(tabExo);
+//exo 7
+tabExo.reverse()
+console.log(tabExo);
+//exo 8
+let boite = [];
+boite.push("objet 1", "objet 2");
+console.log(boite);
+// alert(boiteString);
+boite.unshift('objet 3');
+// alert(boiteString);
+boite.push("objet4", "objet 5", "objet 6");
+console.log(boite);
+// alert(boite)
+boite.splice(0,3);
+console.log(boite);
+boite.push('sdlkfjs')
+// alert(boite);
+boite.splice(0,2);
+console.log(boite);
+boite = [];
+// alert(boite);
+
